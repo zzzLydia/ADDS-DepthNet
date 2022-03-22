5436fb94a09d842f209f66b20a3ffe56d987131a
zzzLydia <73216506+zzzLydia@users.noreply.github.com>
2022-03-22 16:41:54 +0800

    Update __init__.py

--- networks/__init__.py
+++ networks/__init__.py
@@ -3,3 +3,4 @@ from .depth_decoder import DepthDecoder
 from .pose_decoder import PoseDecoder
 from .pose_cnn import PoseCNN
 from .resnet_encoder_pose import ResnetEncoder_pose
+from .SABlock import SABlock
